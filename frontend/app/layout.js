a4f692033f606fe5de88f1d49d4e4f8d6ec5cbc3
Marton Bocsi <mvbocsi@gmail.com>
2024-02-25 02:34:05 -0600

    Pretty bg and navbar

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +13,33 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="absolute flex flex-row w-full justify-between p-10">
+          <Link
+            href="/"
+            className="group rounded-lg border border-transparent px-5 py-3 transition-colors hover:border-gray-300 hover:bg-gray-300 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          >
+            <h2 className={`text-2xl font-semibold`}>Home</h2>
+          </Link>
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://www.premierleague.com/matchweek/12294/blog"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <Image
+              src="/Premier-League-logo.png"
+              alt="PL Logo"
+              className="dark:invert"
+              width={150}
+              height={30}
+              priority
+            />
+          </a>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,35 +3,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 lg:static lg:w-auto">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <div className="z-10 max-w-5xl w-full items-center justify-around font-mono text-sm lg:flex flex-row">
+        <p className="w-full justify-center pb-6 pt-8 lg:static lg:w-auto text-center">
           Get predictions for future games of Premier League&nbsp;
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://www.premierleague.com/matchweek/12294/blog"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            <Image
-              src="/Premier-League-logo.png"
-              alt="PL Logo"
-              className="dark:invert"
-              width={150}
-              height={30}
-              priority
-            />
-          </a>
-        </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/Premier-League-logo-bg.png"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/BackGround-new.png"
           alt="PL Logo"
           width={500}
           height={100}
